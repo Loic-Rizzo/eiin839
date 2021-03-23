13a173a704c789c6e8400bff90c8ac46647b6cd7
Loïc <loic.rizzo@etu.univ-cotedazur.fr>
2021-03-23 15:24:45 +0100

    TD5 REST and SOAP service

---
+++ TD5/MathsLibrary/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathsOperations.IMathsOperations mathsOperations = new MathsOperations.MathsOperationsClient();
+            Console.WriteLine("--- 8 + 12 ---");
+            Console.WriteLine(mathsOperations.Add(8, 12));
+            Console.WriteLine("--- 20 - 10 ---");
+            Console.WriteLine(mathsOperations.Subtract(20, 10));
+            Console.WriteLine("--- 5 * 2.5 ---");
+            Console.WriteLine(mathsOperations.Multiply(5, 2.5));
+            Console.WriteLine("--- 20 / 2 ---");
+            Console.WriteLine(mathsOperations.Divide(50, 2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TD5/MathsLibrary/MathsLibrary/MathsOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class MathsOperations : IMathsOperations
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new Exception("Divise by 0 !");
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+    }
+}
