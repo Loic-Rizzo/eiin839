e7b3e8e08ea5aab4c9cae7f7a5b48640d1d50690
Loïc <loic.rizzo@etu.univ-cotedazur.fr>
2021-03-30 15:00:55 +0200

    Session 5 ABC Model

--- TD5/MathsLibrary/Client/Program.cs
+++ TD5/MathsLibrary/Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client.MathsOperations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,36 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            MathsOperations.IMathsOperations mathsOperations = new MathsOperations.MathsOperationsClient();
-            Console.WriteLine("--- 8 + 12 ---");
-            Console.WriteLine(mathsOperations.Add(8, 12));
-            Console.WriteLine("--- 20 - 10 ---");
-            Console.WriteLine(mathsOperations.Subtract(20, 10));
-            Console.WriteLine("--- 5 * 2.5 ---");
-            Console.WriteLine(mathsOperations.Multiply(5, 2.5));
-            Console.WriteLine("--- 20 / 2 ---");
-            Console.WriteLine(mathsOperations.Divide(50, 2));
+            Console.WriteLine(" MathsOperations Client pour SoapEndPnt1 ");
+            MathsOperationsClient mathsOperationsClient_1 = new MathsOperationsClient("SoapEndPnt1");
+            simpleTest(mathsOperationsClient_1);
+            mathsOperationsClient_1.Close();
+
+
+            Console.WriteLine(" MathsOperations Client pour SoapEndPnt2 ");
+            MathsOperationsClient mathsOperationsClient_2 = new MathsOperationsClient("SoapEndPnt2");
+            simpleTest(mathsOperationsClient_2);
+            mathsOperationsClient_2.Close();
+
+
+            Console.WriteLine(" MathsOperations Client pour SoapEndPnt3 ");
+            MathsOperationsClient mathsOperationsClient_3 = new MathsOperationsClient("SoapEndPnt3");
+            simpleTest(mathsOperationsClient_3);
+            mathsOperationsClient_3.Close();
+
             Console.ReadLine();
+        }
+
+        private static void simpleTest(MathsOperationsClient mathsOperationsClient)
+        {
+            Console.WriteLine("--- 8 + 12 ---");
+            Console.WriteLine(mathsOperationsClient.Add(8, 12));
+            Console.WriteLine("--- 20 - 10 ---");
+            Console.WriteLine(mathsOperationsClient.Subtract(20, 10));
+            Console.WriteLine("--- 5 * 2.5 ---");
+            Console.WriteLine(mathsOperationsClient.Multiply(5, 2.5));
+            Console.WriteLine("--- 20 / 2 ---");
+            Console.WriteLine(mathsOperationsClient.Divide(50, 2));
         }
     }
 }
